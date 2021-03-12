9e2171251ab15421a3c76d15085f68f11df61360
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-12 02:02:09 +0100

    Update read_file.py
    Simple MENU within continous loop has been added.

--- read_file.py
+++ read_file.py
@@ -12,17 +12,35 @@ def read_file(x,y):
     test_file = pd.read_csv(x, sep='\t', decimal=y, index_col = 0)
     return test_file
 
-# Get file path
-path = str(input('\nSpecify file path:\n'))
-#path = 'test.txt'
+# Welcome message
+print('\nWelcome to program!')
 
-# Specify decimal separator in the file (, or .)
-decimal = str(input('\nSpecify decimal separator in the file:\n'))
-#decimal = ','
+# Continous loop of program
+while True:
 
-# Get and print data
-try:
-    data = read_file(path, decimal)
-    print('\n',data,'\n')
-except FileNotFoundError:
-    print('\nSpecified file does not exist or file path is incorrect!\n')
+    # MENU
+    menu = str(input('\nMENU:\n1 - Import file to program\n2 - Option unavailable yet\n3 - Option unavailable yet\n4 - Close program\n\nSelect one of the above by entering suitable number: '))
+
+    if menu == '4':
+        break
+    elif menu == '1':
+        while True:
+            # Get file path
+            path = str(input('\nSpecify file path:\n'))
+            #path = 'test.txt'
+
+            # Specify decimal separator in the file (, or .)
+            decimal = str(input('\nSpecify decimal separator in the file:\n'))
+            #decimal = ','
+
+            # Get and print data
+            try:
+                data = read_file(path, decimal)
+                print('\n',data,'\n')
+                break
+            except FileNotFoundError:
+                back = str(input('\nSpecified file does not exist or file path is incorrect!\nType X to exit to menu   or   press ENTER to continue...'))
+                if back == 'x' or back == 'X':
+                    break
+    else:
+        input('\nIncorrect option!\nPress ENTER to continue...')
