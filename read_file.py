41b47bdc12b2a172148fb93f8f082c8615353f93
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-12 01:10:48 +0100

    read_file.py update
    Manual select of file path and decimal separator is added. Also error handling got incorporated.

--- read_file.py
+++ read_file.py
@@ -6,12 +6,23 @@ import pandas as pd
 #import numpy as np
 #import matplotlib as mtp 
 
-# Reading File
-def read_file(x):
-    test_file = pd.read_csv(x, sep='\t', decimal=',') # "index_col = 0" deleting 1st column with iteration
+
+# Read file
+def read_file(x,y):
+    test_file = pd.read_csv(x, sep='\t', decimal=y, index_col = 0)
     return test_file
 
-path = 'test.txt'
-data = read_file(path)
+# Get file path
+path = str(input('\nSpecify file path:\n'))
+#path = 'test.txt'
 
-print(data)
+# Specify decimal separator in the file (, or .)
+decimal = str(input('\nSpecify decimal separator in the file:\n'))
+#decimal = ','
+
+# Get and print data
+try:
+    data = read_file(path, decimal)
+    print('\n',data,'\n')
+except FileNotFoundError:
+    print('\nSpecified file does not exist or file path is incorrect!\n')
