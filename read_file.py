58a74c055b2563f62586bbbbfca699ab4969f5ab
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-22 21:53:25 +0100

    Update read_file.py
    Minor changes of variables

--- read_file.py
+++ read_file.py
@@ -6,12 +6,10 @@ import pandas as pd
 import pathlib
 
 # Predefined settings
-data_loaded = False         # no data is loaded at the beggining of program
 
 # FUNCTIONS
 # Import file
 def import_file():
-    global data
     while True:
         #data_path = input('\nSpecify folder path (data_test_folder/Pomiar1):\n')
         data_path = 'data_test_folder/Pomiar 1'
@@ -32,15 +30,15 @@ def import_file():
             data =[]
 
             for i in file_names:
-                j = pathlib.Path(i) 
-                if j.exists() == True:
+                check = pathlib.Path(i) 
+                if check.exists() == True:
                     data.append(pd.read_csv(i, sep ='\t', decimal =coma, header =0))
                     loaded_files.append(i)
 
             print('\nFiles loaded:')
             for count, item in enumerate(loaded_files, 0):
                 print(count, item)
-            return data
+            return data, loaded_files
                     
         else:
             input('\nSpecified path does not exist!\nPress ENTER to continue...')
