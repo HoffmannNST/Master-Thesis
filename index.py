1c80185bbb341278bedbf92adb76dc3bf3049b0f
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-23 01:35:03 +0100

    Update index.py
    New calculate() function has been added. Unnecessary comments and imports have been removed.

--- index.py
+++ index.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Import packages
-import pandas as pd
-import pathlib
-# import matplotlib as mtp
-
 # Import functions from additional files
 from read_file import import_file
-#from calculate_file import calculator
+from calculate_file import calculate
 
-# Predefined settings
-calc_set = 'index'          # working with columns indexes or names
-data_loaded = False         # no data is loaded at the beggining of program
-data = []
-
-            
 # START XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # Welcome message
 print('\nWelcome to program!')
 
 # Import file
-data = import_file()
+data, loaded_files = import_file()
 
 # Calculator
-#data = calculator(data)
+data = calculate(data, loaded_files)
 
 # Option 3
 #option3()
+
+input('\n\nPRESS ENTER...')
