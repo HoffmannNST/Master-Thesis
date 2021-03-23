13e4a3ffc5b9e61c6f2cea7e5d7abb3e85c7b898
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-23 01:38:08 +0100

    Update calculate_file.py
    Brand new calculate() function has been built to work without menu. New function performs mathematical operations on DataFrames to achive data suited for plotting.

--- calculate_file.py
+++ calculate_file.py
@@ -3,116 +3,32 @@
 
 # Import packages
 import pandas as pd
+import numpy as np
 
 # Predefined settings
-calc_set = 'index'          # working with columns indexes or names
-data_loaded = False         # no data is loaded at the beggining of program
-step_back = ['x','X']       # keywords to break current loop and go back to previous menu
-close_program = ['x','X']   # keywords to exit program from the MENU
-data = pd.Series()
+data = []
 
-# FUNCTIONS
-# Choose column to edit in the Calculator
-def choose_column(calc_set: str):
-    if calc_set == 'index':
-        while True:
-            try:
-                chosen_column = input('\nChoose a column to edit: ')
-                if chosen_column in step_back:  # back to the Calculator menu
-                    break
-                chosen_column = int(chosen_column)
-                break
-            except ValueError:
-                print('Faulty input!!')
-    else:
-        chosen_column = (input('\nChoose a column to edit: '))
-    return chosen_column
+# FUNCTIONS    
+# Calculate
+def calculate(data, loaded_files):
+    x = -1
+    for i in loaded_files:
+        x += 1
+        data_temp = data[x]
+        new_data = data_temp.loc[:,['Temperatura [K]', 'Opor']]
 
-# Operate on data in Calculator
-def calc_operate(calc_set: str, data, operation: str, column: int or str, number: float):
-    if calc_set == 'index':
-        result = data.iloc[:,[column]]
-    else:
-        result = data.loc[:,[column]]
-    
-    if operation == '+':
-        result += number
-    elif operation == '-':
-        result -= number
-    elif operation == '*':
-        result *= number
-    elif operation == '/':
-        result /= number
-    elif operation == '**':
-        result **= number
-    return result
+        new_data['Ln(1/R)'] = np.log(1/new_data['Opor'])
 
-# Calculator settings
-def calculator_settings():
-    while True:
-        calc_set = input('Do you want to work with column indexes or names (enter i or n): ')
-        if calc_set in step_back:   # Back to MENU
-            break
-        elif calc_set == 'i':
-            return 'index'
-        elif calc_set == 'n':
-            return 'name'
-
-# Calulate
-def calculator(data_loaded, calc_set, data):
-    while True:
-        # Test if there is any file imported to program
-        if data_loaded == False:    
-            input('\nNo data has been loaded yet! Please imoprt a file.\nPress ENTER to continue...')
-            break
-            
-        # Menu for calculator options
-        menu_calc = input('\nCALCULATOR:\n1 - Edit a column\n2 - Create new column\n3 - Settings\nX - Exit to menu\n\nSelect one of the above by entering suitable number: ')
-            
-        if menu_calc in step_back:  # Back to MENU
-            break
-
-        elif menu_calc == '1':      # Edit column
-            edit_column = choose_column(calc_set)       # Select column to edit
-            while True:
-                if edit_column in step_back:            # back to the Calculator menu
-                    break
-
-                calc_operation = input('Choose operation (+, -, *, /, **): ')
-                if calc_operation in step_back:
-                            break
-
-                calc_number = float(input('Choose a number to make operation with (i.e. -2.143): '))
-                if calc_operation == '/' and calc_number == 0:
-                    while calc_number == 0:
-                        print('Division by 0!!!')
-                        calc_number = float(input('Choose a number to make operation with (i.e. -2.143): '))
-                        if calc_number in step_back:
-                            break
-                if calc_number in step_back:
-                            break
-                try:
-                    if calc_set == 'index':
-                        print('\nDATA BEFORE\n', data.iloc[[0,1,2,3],[edit_column]])    #line for testing, to be deleted later
-                        data.iloc[:,[edit_column]] = calc_operate(calc_set, data, calc_operation, edit_column, calc_number)
-                        print('\nDATA AFTER\n', data.iloc[[0,1,2,3],[edit_column]])     #line for testing, to be deleted later
-                    elif calc_set == 'name':
-                        print('\nDATA BEFORE\n', data.loc[[0,1,2,3],[edit_column]])     #line for testing, to be deleted later
-                        data.loc[:,[edit_column]] = calc_operate(calc_set, data, calc_operation, edit_column, calc_number)
-                        print('\nDATA AFTER\n', data.loc[[0,1,2,3],[edit_column]])      #line for testing, to be deleted later
-                except IndexError:
-                    print('Column index out of range!\n')
-                except KeyError:
-                    print('Column label not found!\n')
-
-        elif menu_calc == '2':      # Create new column
-            pass
-
-        elif menu_calc == '3':      # Settings
-            calc_set = calculator_settings()
-
-        else:
-            input('\nIncorrect option!\nPress ENTER to continue...')
-
-    return calc_set, data
+        try:
+            p_step = float(input('Enter the step for the p parameter (i.e. 0.1): '))
+            #p_step = 0.1
+        except:
+            print('Invalid input!')
+            p_step = 0.1
         
+        for p in np.arange(0+p_step, 1+p_step, p_step):
+            new_data[str(round(p,3))] = 1/(new_data['Temperatura [K]']**p)
+        print('\nData from file:\n%s\n\n%s' %(i,new_data))
+        data[x] = new_data
+
+    return data
