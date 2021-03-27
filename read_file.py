d83f4467f0495fd9a8e3cc8024de4f7d1ab3ffde
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-27 03:08:20 +0100

    Update read_file.py
    New option for choosing delimeter in files and slight variables changes.

--- read_file.py
+++ read_file.py
@@ -13,14 +13,14 @@ data =[]
 # Import file
 def import_file():
     while True:
-        data_path = input('\nSpecify folder path (data_test_folder/Pomiar1):\n')
-        #ata_path = 'data_test_folder/Pomiar 1'
+        #data_path = input('\nSpecify folder path (data_test_folder/Pomiar1):\n')
+        data_path = 'data_test_folder/Pomiar 1'
 
-        files_path = pathlib.Path(data_path)
-
-        if files_path.exists() == True:
-            coma = input('\nSpecify decimal separator in the files:\n')     # '.' or ','
-            #coma = ','
+        if pathlib.Path(data_path).exists() == True:
+            #delimeter = input('\nSpecify columns separator (i.e. symbol [, ; .] or \t for tab):\n')     # /t - tab
+            delimeter = '\t'
+            #coma = input('\nSpecify decimal separator in the files:\n')     # '.' or ','
+            coma = ','
 
             data_path1 = data_path+'/Grzanie/grzaniePomiar_Keithley2400.txt'
             data_path2 = data_path+'/Grzanie/grzaniePomiar_Keithley2000.txt'
@@ -29,17 +29,15 @@ def import_file():
 
             file_names = [data_path1, data_path2, data_path3, data_path4]
             
-
             for i in file_names:
-                check_file = pathlib.Path(i) 
-                if check_file.exists() == True:
-                    data.append(pd.read_csv(i, sep ='\t', decimal =coma, header =0))
+                if pathlib.Path(i).exists() == True:
+                    data.append(pd.read_csv(i, sep =delimeter, decimal =coma, header =0))
                     loaded_files.append(i)
 
             print('\nFiles loaded:')
             for count, item in enumerate(loaded_files, 0):
                 print(count, item)
-            return data, loaded_files
+            return data, loaded_files, delimeter, coma
                     
         else:
             input('\nSpecified path does not exist!\nPress ENTER to continue...')
