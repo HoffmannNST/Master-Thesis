7c5512169c754837f73d999d8b3686c97b613336
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-03-29 23:42:31 +0200

    Update write_file.py
    Program now is saving r^2(p) data to r2p.txt file

--- write_file.py
+++ write_file.py
@@ -7,16 +7,19 @@ import pathlib
 
 # FUNCTIONS
 # Import file
-def save_file(data, data_names, delimeter :str, coma :str):
+def save_file(data, data_names, delimeter :str, coma :str, r2p):
     x = -1
     saved_files = []
     
     pathlib.Path('Results/').mkdir(parents=True, exist_ok=True)
 
+    r2p.to_csv('Results/r2p.txt', index=False, sep =delimeter, decimal =coma)
+    saved_files.append('Results/r2p.txt')
+
     for i in data_names:
         x += 1
         data_temp = data[x]
-        data_path = 'Results/' + i + '.txt'     # .txt can be changed to .csv or other format
+        data_path = 'Results/' + i + '.txt'     
         data_temp.to_csv(data_path, index=False, sep =delimeter, decimal =coma)
     
         saved_files.append(data_path)
