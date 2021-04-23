14fd58924ee5d6dc1579987e85b52654584e6429
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-04-24 00:25:14 +0200

    Update index.py
    Preparing DAE calulation function

--- index.py
+++ index.py
@@ -3,9 +3,9 @@
 
 # Import functions from additional files
 from read_file import import_file
-from calculate_file import calculate
+from calculate_file import calculate_arrhenius, calculate_dae
 from write_file import save_file
-from plot_file import do_plot
+from plot_file import make_plot
 
 # START XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 # Welcome message
@@ -14,13 +14,16 @@ print('\nWelcome to program!')
 # Import file
 data, delimeter, coma, data_names = import_file()
 
-# Calculator
-data, r2p = calculate(data, data_names)
+# Calculator Arrhenius
+data, r2p = calculate_arrhenius(data, data_names)
+
+#Calculator DAE
+calculate_dae(data, data_names)
 
 # Save file
 save_file(data, data_names, delimeter, coma, r2p)
 
 # Plot
-do_plot(r2p, data_names, data)
+make_plot(r2p, data_names, data)
 
 input('\n\nPRESS ENTER...')
