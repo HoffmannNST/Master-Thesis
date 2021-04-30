71ce493ba825982405fe0cc95f5c0b056bf5f903
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-04-30 02:05:49 +0200

    Update calculate_file.py
    New feature, for calculating T0 and R0 parameters from R(T) data in dependace of r^2(p) function, has been added. Changes to variable names

--- calculate_file.py
+++ calculate_file.py
@@ -4,8 +4,8 @@
 # Import packages
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from math import exp
+from scipy import stats
 
 # Predefined settings
 data = []
@@ -26,7 +26,10 @@ def calculate_arrhenius(data, data_names):
     for p in np.arange(0+p_step, 1+p_step, p_step):
         p_list.append(round(p,3))
 
-    r2p = pd.DataFrame(p_list, columns = ['p'])     # r2p stands for function r^2(p)
+    # Tables of calculated parameters
+    r2_table = pd.DataFrame(p_list, columns = ['p'])    # r2 is correlation coeaficient
+    T0_table = pd.DataFrame(p_list, columns = ['p'])    # T0 is characteristic temp. deriviated from slope of function
+    R0_table = pd.DataFrame(p_list, columns = ['p'])    # R0 is a pre-exponential factor deriviated from intercept of function
 
     for i in data_names:
         x += 1
@@ -35,32 +38,44 @@ def calculate_arrhenius(data, data_names):
         new_data = temp_data.loc[:,['Temperatura [K]', 'Opor']]
         new_data['Ln(1/R)'] = np.log(1/new_data['Opor'])
         
-        regr = LinearRegression()
-        Y = new_data['Ln(1/R)'].values.reshape(-1,1)
-        r2_scores = []
+        Y = new_data['Ln(1/R)']
+        r2_list = []
+        T0_list = []
+        R0_list = []
 
         for p in np.arange(0+p_step, 1+p_step, p_step):
             column_p_name = '1/T^(' + str(round(p,3)) + ')'
             new_data[column_p_name] = 1/(new_data['Temperatura [K]']**p)
-            X = new_data[column_p_name].values.reshape(-1,1)
-            regr.fit(X, Y)
-            score = regr.score(X, Y)
-            r2_scores.append(score)
-        
-        r2p[i] = r2_scores
+            X = new_data[column_p_name]
+            slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)
+            del p_value
+            del std_err
+            r2_list.append(r_value**2)
+            slope *= -1
+            slope **= 1/p
+            T0_list.append(slope)
+            intercept = exp(-intercept)
+            R0_list.append(intercept)
+
+        r2_table[i] = r2_list
+        T0_table[i] = T0_list
+        R0_table[i] = R0_list
         data[x] = new_data
 
-    print('\nCalculated r^2(p):\n', r2p)
+    print('\nCalculated r^2(p):\n', r2_table)
+    print('\nCalculated T0 parameter a:\n', T0_table)
+    print('\nCalculated R0 parameter:\n', R0_table)
 
-    print('\nMax values of r^2(p):\n')
+    print('\nMax values of r^2(p):')
     for i,j in enumerate(data_names):
-        find_max_p = r2p[j]
-        index_p = find_max_p.idxmax()
-        max_r2 = find_max_p[index_p]
-        max_p = r2p.iloc[0,index_p]
-        print('%s. max r^2 = %.3f for p = %.2f for data %s' % (i+1,max_r2,max_p,j))
+        index_p = r2_table[j].idxmax()
+        max_r2 = r2_table.iloc[index_p,i+1]
+        param_p = r2_table.iloc[index_p,0]
+        param_T0 = T0_table.iloc[index_p,i+1]
+        param_R0 = R0_table.iloc[index_p,i+1]
+        print('%s. max r^2 = %.3f for p = %.2f, parameters T0 = %.2f, R0 = %.2f, for data %s' % (i+1,max_r2,param_p,param_T0,param_R0,j))
 
-    return data, r2p
+    return data, r2_table
 
 def calculate_dae():
     pass
