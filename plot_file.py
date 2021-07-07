665739660f27447699ae5bd1ea189c140d0cb7ff
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-07-07 04:33:02 +0200

    Update plot_file.py

--- plot_file.py
+++ plot_file.py
@@ -4,89 +4,65 @@
 # Import packages
 import pandas as pd
 import matplotlib.pyplot as plt
+import pathlib
 
 # FUNCTIONS    
 # Calculate
-def make_plot(r2_table, data_names, data, data_dae):
+def make_plot(data, data_names, data_dae, r2_table):
+    """Function that makes plots of 3 different functions and saves them as .png files.
+
+    Args:
+        data (list): list of DataFrames of raw data
+        data_names (list): list of names of files imported to program
+        data_dae (list): list of DataFrames of calculted data
+        r2_table (pandas.DataFrame): table of r^2 (cube of pearson coeficient) values
+    """
+    pathlib.Path('Results/Plots').mkdir(parents=True, exist_ok=True)
+
+    # Plot r^2(p)
     X = r2_table.iloc[:, 0]
-
-    # Plot Arrhenius r^2(p)
-    fig1, axsrp = plt.subplots(2,2)
-    fig1.suptitle('Arrhenius r^2(p)')
-    axsrp[0,0].scatter(X, r2_table.iloc[:, 1])
-    axsrp[0,0].set_title(data_names[0])
-    try:
-        axsrp[0,1].scatter(X, r2_table.iloc[:, 2])
-        axsrp[0,1].set_title(data_names[1])
-    except:
-        pass
-    try:
-        axsrp[1,0].scatter(X, r2_table.iloc[:, 3])
-        axsrp[1,0].set_title(data_names[2])
-    except:
-        pass
-    try:
-        axsrp[1,1].scatter(X, r2_table.iloc[:, 4])
-        axsrp[1,1].set_title(data_names[3])
-    except:
-        pass
-
-    for ax in axsrp.flat:
-        ax.set(xlabel='p', ylabel='r^2')
-    fig1.tight_layout()
+    for count, item in enumerate(data_names, 1):
+        Y = r2_table.iloc[:,count]
+        plt.xlabel('p')
+        plt.ylabel('r$^{2}$')
+        plt.title(item, {'horizontalalignment': 'center'})
+        plt.suptitle('Arrhenius r$^{2}$(p)')
+        plt.scatter(X,Y) 
+        file_path = 'Results/Plots/r2_' + item + '.png'
+        plt.savefig(file_path, dpi=300)
+        #plt.show()
+        plt.close()
 
     # Plot raw data R(T)
-    fig2, axsRT = plt.subplots(2,2)
-    fig2.suptitle('R(T)')
-    x = -1
-    for i in data_names:
-        x += 1
-        temp_data = data[x]
-        if x == 0:
-            axsRT[0,0].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 1])
-            axsRT[0,0].set_title(i)
-
-        if x == 1:
-            axsRT[0,1].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 1])
-            axsRT[0,1].set_title(i)
-            
-        if x == 2:
-            axsRT[1,0].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 1])
-            axsRT[1,0].set_title(i)
-            
-        if x == 3:
-            axsRT[1,1].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 1])
-            axsRT[1,1].set_title(i)
-
-        for ax in axsRT.flat:
-            ax.set(xlabel='T [Kelvin]', ylabel='R [Ohm]')
-        fig2.tight_layout()
+    for count, item in enumerate(data_names, 0):
+        temporary_data = data[count]
+        X = temporary_data.iloc[:, 0]
+        Y = temporary_data.iloc[:, 1]
+        plt.xlabel('T [Kelvin]')
+        plt.ylabel('R [Ohm]')
+        plt.title(item, {'horizontalalignment': 'center'})
+        plt.suptitle('R(T)')
+        plt.scatter(X,Y) 
+        file_path = 'Results/Plots/RT_' + item + '.png'
+        plt.savefig(file_path, dpi=300)
+        #plt.show()
+        plt.close()
 
     # Plot DAE(T)
-    fig3, axsDAE = plt.subplots(2,2)
-    fig3.suptitle('DAE(T)')
-    x = -1
-    for i in data_names:
-        x += 1
-        temp_data = data_dae[x]
-        if x == 0:
-            axsDAE[0,0].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 6])
-            axsDAE[0,0].set_title(i)
+    for count, item in enumerate(data_names, 0):
+        temporary_data = data_dae[count]
+        X = temporary_data.iloc[:, 0]
+        Y = temporary_data.iloc[:, 5]
+        plt.xlabel('T [Kelvin]')
+        plt.ylabel('DAE [eV]')
+        plt.title(item, {'horizontalalignment': 'center'})
+        plt.suptitle('DAE(T)')
+        plt.scatter(X,Y) 
+        file_path = 'Results/Plots/DAE_' + item + '.png'
+        plt.savefig(file_path, dpi=300)
+        #plt.show()
+        plt.close()
 
-        if x == 1:
-            axsDAE[0,1].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 6])
-            axsDAE[0,1].set_title(i)
-            
-        if x == 2:
-            axsDAE[1,0].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 6])
-            axsDAE[1,0].set_title(i)
-            
-        if x == 3:
-            axsDAE[1,1].scatter(temp_data.iloc[:, 0], temp_data.iloc[:, 6])
-            axsDAE[1,1].set_title(i)
 
-        for ax in axsDAE.flat:
-            ax.set(xlabel='T [Kelvin]', ylabel='DAE [eV]')
-        fig3.tight_layout()
-
-    plt.show()
+    
+    # ZMIENIĆ PLOTOWANIE NA UKŁADANIE WYKRESÓW PLIKAMI, A NIE TYPEM DANYCH !!!
