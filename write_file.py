2a32ee050057869bd27b8014023be3567c568ef6
Nicolas <72359832+HoffmannNST@users.noreply.github.com>
2021-07-07 04:33:54 +0200

    Update write_file.py

--- write_file.py
+++ write_file.py
@@ -5,36 +5,63 @@
 import pathlib
 
 # FUNCTIONS
-# Export file
-def save_file(data_arr, data_dae, data_names, delimeter :str, coma :str, r2_table):
+def save_arrhenius(data_arrhenius, data_names, delimeter :str, coma :str, r2_table):
+    """Function that saves data calculated in calcualte_arrhenius function to .txt files.
+
+    Args:
+        data_arr (list): list of DataFrames of calculted data
+        data_names (list): list of names of files imported to program
+        delimeter (str): separator of columns in data file
+        coma (str): decimal sign in numbers
+        r2_table (pandas.DataFrame): table of r^2 (cube of pearson coeficient) values
+
+    Returns:
+        list: list of saved files
+    """
     x = -1
     saved_files = []
 
     pathlib.Path('Results/Arrhenius').mkdir(parents=True, exist_ok=True)
-    pathlib.Path('Results/DAE').mkdir(parents=True, exist_ok=True)
 
-    # Save Arrhenius data
     r2_table.to_csv('Results/Arrhenius/r2p_arr.txt', index=False, sep =delimeter, decimal =coma)
     saved_files.append('Results/Arrhenius/r2p_arr.txt')
     for i in data_names:
         x += 1
-        data_temp = data_arr[x]
+
+        temporary_data = data_arrhenius[x]
         data_path = 'Results/Arrhenius/' + i + '.txt'     
-        data_temp.to_csv(data_path, index=False, sep =delimeter, decimal =coma)
+        temporary_data.to_csv(data_path, index=False, sep =delimeter, decimal =coma)
     
         saved_files.append(data_path)
+    return saved_files
 
-    # Save DAE data
+def save_dae(data_dae, data_names, delimeter :str, coma :str, saved_files):
+    """Function that saves data calculated in calcualte_dae function to .txt files.
+
+    Args:
+        data_dae (list): list of DataFrames of calculted data
+        data_names (list): list of names of files imported to program
+        delimeter (str): separator of columns in data file
+        coma (str): decimal sign in numbers
+        saved_files (list): list of saved files
+
+    Returns:
+        [list]: updated list of saved files
+    """
     x = -1
+
+    pathlib.Path('Results/DAE').mkdir(parents=True, exist_ok=True)
+
     for i in data_names:
         x += 1
-        data_temp = data_dae[x]
+        temporary_data = data_dae[x]
         data_path = 'Results/DAE/' + i + '.txt'     
-        data_temp.to_csv(data_path, index=False, sep =delimeter, decimal =coma)
+        temporary_data.to_csv(data_path, index=False, sep =delimeter, decimal =coma)
     
         saved_files.append(data_path)
 
+    # to be removed
     print('\nFiles saved:')
     for count, item in enumerate(saved_files, 0):
         print(count, item)
-    return
+    return saved_files
